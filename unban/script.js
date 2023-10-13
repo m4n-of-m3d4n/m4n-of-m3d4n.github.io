62bbbae602f412e769de250df4936776371be357
claudiobosetti <claudiobosetti1@gmail.com>
2023-10-13 22:49:09 +0200

    unban form added

---
+++ unban/script.js
@@ -0,0 +1,25 @@
+(function() {
+    emailjs.init("EVZcOVrAg0gIq_kvg"); 
+})();
+
+function inviaRichiesta(event) {
+    event.preventDefault();
+
+    const formData = {
+        name: document.getElementById('name').value,
+        telegramNick: document.getElementById('telegramNick').value,
+        telegramID: document.getElementById('telegramID').value,
+        banReason: document.getElementById('banReason').value,
+        unbanMotivation: document.getElementById('unbanMotivation').value,
+        acceptance: document.getElementById('acceptance').checked ? 'Accettato' : 'Non accettato'
+    };
+
+    emailjs.send('service_0nknsu3', 'template_jl082ae', formData)
+        .then(function(response) {
+            alert('Email inviata con successo!');
+        }, function(error) {
+            alert('Errore durante l\'invio dell\'email: ' + JSON.stringify(error));
+        });
+}
+
+document.getElementById('unbanForm').addEventListener('submit', inviaRichiesta);
